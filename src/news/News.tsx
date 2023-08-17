7cfc1c98136d05677fc1453dc21e5e0ab3d52f73
Eytan3000 <120247900+Eytan3000@users.noreply.github.com>
2023-08-17 08:48:26 +0300

    json api working

--- src/news/News.tsx
+++ src/news/News.tsx
@@ -1,25 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Box, Button, Card, Sheet, Skeleton, Typography } from '@mui/joy';
 import Link from '@mui/joy/Link';
 import AspectRatio from '@mui/joy/AspectRatio';
 import { yahooFinanceLogo } from '../utils/links';
-import { env } from 'process';
+import { yahooNewsApi } from '../utils/api';
+import axios from 'axios';
 //---------------------------------------
-type RequestOptions = {
-  method: string;
-  url: string;
-  params: {
-    region: string;
-    snippetCount: string;
-  };
-  headers: {
-    'content-type': string;
-    'X-RapidAPI-Key': string | undefined;
-    'X-RapidAPI-Host': string;
-  };
-  data: string;
-};
+// type RequestOptions = {
+//   method: string;
+//   url: string;
+//   params: {
+//     region: string;
+//     snippetCount: string;
+//   };
+//   headers: {
+//     'content-type': string;
+//     'X-RapidAPI-Key': string | undefined;
+//     'X-RapidAPI-Host': string;
+//   };
+//   data: string;
+// };
 
 interface NewsItem {
   content: {
@@ -42,40 +42,30 @@ interface NewsItem {
   id: string;
 }
 
-const apiKey = process.env.REACT_APP_YAHOO_API_KEY;
-const options = {
-  method: 'POST',
-  url: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/news/v2/list',
-  params: {
-    region: 'US',
-    snippetCount: '28',
-  },
-  headers: {
-    'content-type': 'text/plain',
-    'X-RapidAPI-Key': apiKey,
-    'X-RapidAPI-Host': 'apidojo-yahoo-finance-v1.p.rapidapi.com',
-  },
-  data: 'Pass in the value of uuids field returned right in this endpoint to load the next page, or leave empty to load first page',
-};
 //---------------------------------------
 export default function News() {
   const [newsData, setNewsData] = useState<NewsItem[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
-
+  //AxiosResponse<any, any>
   useEffect(() => {
-  
-    (async function news(options: RequestOptions) {
+    (async function () {
       try {
         setLoading(true);
-        const response = await axios.request(options);
-        const data = response.data.data.main.stream;
-        setNewsData(data); //newsArray
+        // Real API:
+        // const response = await yahooNewsApi();
+        // const data = response.data.data.main.stream;
+        // setNewsData(data); //newsArray
+
+        // json api
+        const response = await axios.get('/data/YahooFinance.json');
+        setNewsData(response.data);
+
       } catch (error) {
         console.error(error);
       } finally {
         setLoading(false);
       }
-    })(options);
+    })();
   }, []);
   console.log(newsData);
   console.log(loading);
@@ -87,24 +77,12 @@ export default function News() {
       </Typography>
 
       <Box display={'flex'} justifyContent={'center'}>
-      
-        {loading ? 
-        newsData.map((newsItem, index) => {
-          if (index < 3) {
-        return (
-          <Card
-                sx={{ width: 300, margin: 4, height: 350 }}
-                key={index}
-                variant="outlined">
-        <Skeleton variant="rectangular"  width={'100%'} height={'100%'}/>
-        </Card>
-        )
-          }})
-        : newsData.map((newsItem, index) => {
-          if (newsItem.content.thumbnail && index < 3) { //sometimes there's no thumbnail, so checking if there's one.
+        {newsData.map((newsItem, index) => {
+          if (newsItem.content.thumbnail && index < 5) {
+            //sometimes there's no thumbnail, so checking if there's one.
             return (
               <Card
-                sx={{ width: 300, margin: 4, height: '100%' }}
+                sx={{ minWidth: 300, margin: 4, height: '100%' }}
                 key={index}
                 variant="outlined">
                 <AspectRatio
@@ -134,6 +112,7 @@ export default function News() {
               </Card>
             );
           }
+          return null;
         })}
         <img height={20} alt="yahoo finance logo" src={yahooFinanceLogo} />
       </Box>
